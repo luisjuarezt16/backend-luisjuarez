d693a77f87f7674e2a4345c610b4b66afeba753a
Luis Juarez <luisjuarezt16@gmail.com>
2026-03-01 21:45:49 -0300

    agrega pagina de productos con json

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express'
 import { engine } from 'express-handlebars'
 import path from 'path'
 import { fileURLToPath } from 'url'
+import fs from 'fs'
 
 import homerouter from './routes/home.routes.js'
 import servicesRouter from './routes/services.routes.js'
@@ -12,49 +13,61 @@ const __dirname = path.dirname(__filename)
 const app = express()
 const PORT = 3000
 
-/** ===============================
- *  MOTOR DE PLANTILLAS
- * =============================== */
+// motor de plantillas
 
-app.engine('hbs', engine({ extname: '.hbs' }))
+app.engine('hbs', engine({
+    extname: '.hbs',
+    helpers: {
+        formatPrice: (price) => {
+            return new Intl.NumberFormat('es-CL').format(price)
+        }
+    }
+}))
+
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'views'))
 
-/** ===============================
- *  MIDDLEWARES
- * =============================== */
+// middlewares
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-/** ===============================
- *  ARCHIVOS ESTÁTICOS
- * =============================== */
+// archivos estaticos
 
 app.use('/static', express.static(path.join(__dirname, 'public')))
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
-/** ===============================
- *  RUTAS
- * =============================== */
+// rutas principales
 
 app.use('/', homerouter)
 app.use('/services', servicesRouter)
 
-/** ===============================
- *  ERROR 404
- * =============================== */
+// ruta productos
 
-app.use((req, res) => {
-    res.status(404).render('404', {
-        title: 'PAGINA NO ENCONTRADA'
+app.get('/products', (req, res) => {
+
+    const productsPath = path.join(__dirname, 'data', 'products.json')
+
+    const products = JSON.parse(
+        fs.readFileSync(productsPath, 'utf-8')
+    )
+
+    res.render('products', {
+        title: 'productos',
+        products
     })
 })
 
-/** ===============================
- *  SERVIDOR
- * =============================== */
+// error 404
+
+app.use((req, res) => {
+    res.status(404).render('404', {
+        title: 'pagina no encontrada'
+    })
+})
+
+// servidor
 
 app.listen(PORT, () => {
-    console.log(`Servidor escuchando en http://localhost:${PORT}`)
+    console.log(`servidor escuchando en http://localhost:${PORT}`)
 })
